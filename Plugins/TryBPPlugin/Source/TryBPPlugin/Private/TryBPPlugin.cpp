2ea94ee9d5ec32c2eeef0bd20b73055edba0ea74
xuyang <xuyang@wozlla.com>
2015-11-22 19:01:52 +0800

    调整

--- Plugins/TryBPPlugin/Source/TryBPPlugin/Private/TryBPPlugin.cpp
+++ Plugins/TryBPPlugin/Source/TryBPPlugin/Private/TryBPPlugin.cpp
@@ -7,7 +7,7 @@
 void FTryBPPluginModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	IAssetTools& AssetTools = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools").Get();
+	//IAssetTools& AssetTools = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools").Get();
 
 }
 
